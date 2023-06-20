52ac78847df011c688bc00b08c6a60cfc8a26bf6
rk <103226779+rkbno@users.noreply.github.com>
2023-06-20 20:39:42 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ const lixo = document.querySelector('.obstaculo');
 const div = document.querySelector('.game-board');
 const score = document.querySelector('.score')
 let gameStarted = false;
-let count = 0 
- 
+let count = 0;
+let loop = null;
+
 div.addEventListener('click', clicou);
 
 function clicou (){
@@ -18,29 +19,38 @@ const jump = () => {
   bart.classList.add('jump');
   
   setTimeout(() => {
-  bart.classList.remove('jump');
+    bart.classList.remove('jump');
   },1000);
 };
 
-function startGame(){
-  var loop = setInterval(()=>{
+function restartGame(){
+  clearInterval(loop);
+  loop = null;
+  
+  bart.style.bottom = '3%';
+  bart.src = 'imagens/zyro-image.png';
+  bart.style.width = '180px';
+  
+  count = 0;
+  score.innerHTML = `Score: ${count}`;   
+  
+  loop = setInterval(()=>{
     const lixoPosition = lixo.offsetLeft;
     const bartPosition = +window.getComputedStyle(bart).bottom.replace('px', '');
-
+    
     if (lixoPosition <= 80 && bartPosition < 115 && lixoPosition > 0){
       alert(`Game Over! Seu score foi: ${count}`);
-      lixo.style.left = `${lixoPosition}px`;
-      bart.style.bottom = `${bartPosition}px`;
-
-      bart.src = "imagens/1835-512x512.png";
-      bart.style.width = '150px';
-      
-      count = 0;
+      restartGame();
     };
     count++;
     score.innerHTML = `Score: ${count}`;
   }, 10);
-  
+}
+
+function startGame(){
+  restartGame();
+  gameStarted = true;
+
   document.addEventListener('keydown', jump);
 };
 
