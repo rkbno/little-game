b222a5aeb83044da847a0c007e63e0c699620dbb
rk <103226779+rkbno@users.noreply.github.com>
2023-06-20 11:27:29 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,20 +5,14 @@ const lixo = document.querySelector('.obstaculo');
 const nuvem = document.querySelector('.nuvem');
 const btn = document.querySelector('.botao')
 const div = document.querySelector('.game-board')
-let loop = null;
-
-
 
 btn.addEventListener('click', clicou)
-
 function clicou (){
-  if (!loop){
-    startGame();
-    lixo.style.animation = 'lixo-animation 2s infinite linear';
-    nuvem.style.animation = 'nuvem-animation 30s infinite linear';
-    btn.style.opacity = '0';
-    div.style.filter = 'invert(0)'
-  };
+  startGame();
+  lixo.style.animation = 'lixo-animation 2s infinite linear';
+  nuvem.style.animation = 'nuvem-animation 30s infinite linear';
+  btn.style.opacity = '0';
+  div.style.filter = 'invert(0)'
 };
 
 
@@ -32,50 +26,38 @@ function clicou (){
 
 
 
-const btnRestart = document.querySelector('#btn-restart');
 
-function resetGame() {
-  lixo.style.animation = 'lixo-animation 2s infinite linear';
-  nuvem.style.animation = 'nuvem-animation 30s infinite linear';
-  
-  bart.src = "imagens/zyro-image.png";
-  
-  btn.style.opacity = '1';
-  div.style.filter = 'invert(30%)';
-};
-
-btnRestart.addEventListener('click', resetGame);
 
 function startGame(){
+
   const jump = () => {
     bart.classList.add('jump');
+    
     setTimeout(() => {
-      bart.classList.remove('jump');
+    bart.classList.remove('jump');
     },1000);
   };
-  
-  
-  loop = setInterval(()=>{
+
+  const loop = setInterval(()=>{
+
     const lixoPosition = lixo.offsetLeft;
     const bartPosition = +window.getComputedStyle(bart).bottom.replace('px', '');
-    
+
     if (lixoPosition <= 80 && bartPosition < 115 && lixoPosition > 0){
+
       lixo.style.animation = 'none'
       lixo.style.left = `${lixoPosition}px`
+
       bart.style.animation = 'none'
       bart.style.bottom = `${bartPosition}px`
+
       bart.src = "imagens/1835-512x512.png"
       bart.style.width = '150px'
       
-      clearInterval(loop);
-      loop = null;      
-      resetGame();
+      // alert('Infelizmente você perdeu, precione clique para reiniciar o jogo!')
     }
   }, 10)
   
   document.addEventListener('keydown', jump);
 }
-
-
-
 
