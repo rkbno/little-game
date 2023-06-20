1c0fc7bb9d7c37e3e4a3220ad80a9ae240493d42
rk <103226779+rkbno@users.noreply.github.com>
2023-06-20 11:46:14 -0300

    ajustes

--- js/script.js
+++ js/script.js
@@ -3,16 +3,21 @@
 const bart = document.querySelector('.bart');
 const lixo = document.querySelector('.obstaculo');
 const nuvem = document.querySelector('.nuvem');
-const btn = document.querySelector('.botao')
-const div = document.querySelector('.game-board')
+const btn = document.querySelector('.botao');
+const div = document.querySelector('.game-board');
+let gameStarted = false;
 
-btn.addEventListener('click', clicou)
+btn.addEventListener('click', clicou);
+
 function clicou (){
-  startGame();
-  lixo.style.animation = 'lixo-animation 2s infinite linear';
-  nuvem.style.animation = 'nuvem-animation 30s infinite linear';
-  btn.style.opacity = '0';
-  div.style.filter = 'invert(0)'
+  if (!gameStarted) {
+    startGame();
+    lixo.style.animation = 'lixo-animation 2s infinite linear';
+    nuvem.style.animation = 'nuvem-animation 30s infinite linear';
+    btn.style.opacity = '0';
+    div.style.filter = 'invert(0)'
+    gameStarted = true;
+  };
 };
 
 
@@ -39,22 +44,24 @@ function startGame(){
   };
 
   const loop = setInterval(()=>{
-
     const lixoPosition = lixo.offsetLeft;
     const bartPosition = +window.getComputedStyle(bart).bottom.replace('px', '');
 
     if (lixoPosition <= 80 && bartPosition < 115 && lixoPosition > 0){
+      clearInterval(loop);
 
-      lixo.style.animation = 'none'
-      lixo.style.left = `${lixoPosition}px`
+      lixo.style.animation = 'none';
+      lixo.style.left = `${lixoPosition}px`;
 
-      bart.style.animation = 'none'
-      bart.style.bottom = `${bartPosition}px`
+      bart.style.animation = 'none';
+      bart.style.bottom = `${bartPosition}px`;
 
-      bart.src = "imagens/1835-512x512.png"
-      bart.style.width = '150px'
+      bart.src = "imagens/1835-512x512.png";
+      bart.style.width = '150px';
       
       // alert('Infelizmente você perdeu, precione clique para reiniciar o jogo!')
+      btn.style.opacity = '1';
+      gameStarted = false;
     }
   }, 10)
   
